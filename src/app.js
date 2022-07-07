bc2cff7e0c70e9c4ae105f99e7fcc15fe5da1650
YamilMS <yoms.tf@gmail.com>
2022-07-07 02:31:27 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#Motivo").innerHTML = excusa();
+  });
+};
+
+let excusa = () => {
+  let who = ["My cat", "Aliens", "Steve Jobs", "Vladimir Putin", "Pikachu"];
+  let action = ["burned", "broke", "ate", "pissed", "attacked"];
+  let what = ["my homework", "my house", "my car", "my dog", "my mother"];
+  let when = [
+    "yesterday",
+    "before my lunch",
+    "last week",
+    "while I was sleeping",
+    "just a second ago"
+  ];
+  let quien = Math.floor(Math.random() * who.length);
+  let accion = Math.floor(Math.random() * action.length);
+  let que = Math.floor(Math.random() * what.length);
+  let cuando = Math.floor(Math.random() * when.length);
+
+  return (
+    who[quien] + " " + action[accion] + " " + what[quien] + " " + when[cuando]
+  );
 };
